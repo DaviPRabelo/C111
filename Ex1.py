bb8b86bc843f83b91f71351ff926253ff770d64c
DaviPRabelo <160641658+DaviPRabelo@users.noreply.github.com>
2025-02-12 10:15:13 -0300

    Add files via upload

---
+++ Ex1.py
@@ -0,0 +1,11 @@
+
+nome = "Davi Padula Rabelo"
+nomeUp = nome.upper()
+nomeLow = nome.lower()
+qnt = len(nome)
+new = nome.replace("Rabelo", "do Inatel")
+
+print(nomeUp)
+print(nomeLow)
+print(qnt)
+print(new)
